d7117f08d3d956227a25fb6f65a7ee88b8c9e6f2
Julien Stilmant <stilmantjulien@hotmail.com>
2016-02-17 18:31:27 +0100

    serveur

--- essai2.py
+++ essai2.py
@@ -1049,4 +1049,4 @@ def lien():
         </body>
         </html>'''
     return r
-run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+run(host='0.0.0.0', port=int(os.environ.get('PORT', 5005)))
